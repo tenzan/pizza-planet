9ddfd12f03288b05c8c0c97bc7122536d0f94b3d
Askar <askar.karasaev@gmail.com>
2020-01-16 18:30:45 +0900

    Adding the shopping basket

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -31,7 +31,25 @@
         </tbody>
       </table>
     </div>
-    {{ basket }}
+    <!-- shopping basket -->
+    <div class="basket">
+      <h3>~ Basket ~</h3>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <button class="btn_green">&#8722;</button>
+              <span>2</span>
+              <button class="btn_green">&#43;</button>
+            </td>
+            <td>Pepperoni 9"</td>
+            <td>$6.95</td>
+          </tr>
+        </tbody>
+      </table>
+      <p>Order total:</p>
+      <button class="btn_green">Place Order</button>
+    </div>
   </div>
 </template>
 
@@ -118,7 +136,8 @@ h3 {
   flex-direction: column;
 }
 
-.menu {
+.menu,
+.basket {
   background: #f1e6da;
   border-radius: 3px;
   height: 100vh;
@@ -135,5 +154,9 @@ h3 {
 
 .menu {
   width: 65vm;
+}
+
+.basket {
+  width: 35vw;
 }
 </style>
